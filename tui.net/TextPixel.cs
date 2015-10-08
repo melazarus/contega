8709d121787b5f5bb6067b26c160af2e5ac17b32
Hans Polders <hans.polders@gfk.com>
2015-10-08 12:26:46 +0200

    Added TextPixel.cs

---
+++ tui.net/TextPixel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tui.net
+{
+    public class TextPixel : IEquatable<TextPixel>
+    {
+        public char? Character { get; private set; }
+
+        public ConsoleColor? BackgroundColor { get; private set; }
+
+        public ConsoleColor? ForegroundColor { get; private set; }
+
+        public TextPixel(char? character = null, ConsoleColor? backgroundColor = null, ConsoleColor? foregroundColor = null)
+        {
+            BackgroundColor = backgroundColor;
+            Character = character;
+            ForegroundColor = foregroundColor;
+        }
+
+        public bool Equals(TextPixel obj)
+        {
+            return (obj.BackgroundColor.Equals(BackgroundColor) &&
+                    obj.ForegroundColor.Equals(ForegroundColor) &&
+                    obj.Character.Equals(Character));
+        }
+    }
+}

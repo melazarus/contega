b514d236667ecb158fe7f4c9e165fd10ea97d3d2
Hans Polders <hans.polders@gmail.com>
2015-10-09 21:17:52 +0200

    added ITextGraphics Interface

---
+++ tui.net/ITextGraphics.cs
@@ -0,0 +1,16 @@
+﻿namespace tui.net
+{
+    public interface ITextGraphics
+    {
+        /// <summary>
+        /// Return the image object that is linked with this TextGraphics.
+        /// </summary>
+        /// <returns></returns>
+        TextImage GetBufferImage();
+
+        /// <summary>
+        /// Update the display with the content of the merged Text image
+        /// </summary>
+        void Draw();
+    }
+}

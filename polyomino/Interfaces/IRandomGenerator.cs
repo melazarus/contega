a2b528a02c4c874faf1a14f3e2e73960034c080a
Hans Polders <hans@polders.be>
2015-10-12 21:38:46 +0200

    comments

--- polyomino/Interfaces/IRandomGenerator.cs
+++ polyomino/Interfaces/IRandomGenerator.cs
@@ -1,11 +1,27 @@
 ﻿namespace Contega.Interfaces
 {
+    /// <summary>
+    /// Generate random numbers in a bag.
+    /// Start over when the bag is empty.
+    /// This ensures that two same numbers are never more than a certain distance away.
+    /// </summary>
     public interface IRandomGenerator
     {
+        /// <summary>
+        /// Reset the bag.
+        /// </summary>
         void Reset();
 
+        /// <summary>
+        /// Pick the next item from tha bag.
+        /// </summary>
+        /// <returns></returns>
         int Next();
 
+        /// <summary>
+        /// Peek at the next item in the bag
+        /// </summary>
+        /// <returns></returns>
         int Peek();
     }
 }

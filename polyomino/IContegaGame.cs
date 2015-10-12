00f2ca8478821aef0239db8c3b2cf6f26862b1ca
Hans Polders <hans.polders@gfk.com>
2015-10-12 11:17:29 +0200

    update IContegaGame.cs

--- polyomino/IContegaGame.cs
+++ polyomino/IContegaGame.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿//TODO: allow swap current block with next block
+//TODO: allow initial seed of grid with random data or with predefined set of data
+//TODO: allow lines to be added on the bottom 
 namespace polyomino
 {
     public interface IContegaGame
@@ -15,14 +12,40 @@ namespace polyomino
         int Tick();
 
         /// <summary>
-        /// Reset the game to its initial state
+        /// Reset the game to its initial state (score, level, grid, next block)
         /// </summary>
         void Reset();
 
+        /// <summary>
+        /// Try to rotate the active Tetromino clockwise or counter clockwise
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <returns>True if success</returns>
+        bool TryRotate(object direction);
+
+        /// <summary>
+        /// A 2d array of tetromio enums
+        /// </summary>
         object Grid { get; }
 
+        /// <summary>
+        /// The current score
+        /// </summary>
         int Score { get; }
 
+        /// <summary>
+        /// The level of the game. Starts at 1.
+        /// </summary>
         int Level { get; }
+
+        /// <summary>
+        /// Indicate if the game is over or not
+        /// </summary>
+        bool GameOver { get; }
+
+        /// <summary>
+        /// The the next type of tetromino
+        /// </summary>
+        object NextBlock { get; }
     }
 }

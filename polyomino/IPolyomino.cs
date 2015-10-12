d385a7ea7566a0c7ca6d2e7eb7579020acdd10b4
Hans Polders <hans.polders@gfk.com>
2015-10-12 12:59:44 +0200

    Added IPolyomino

---
+++ polyomino/IPolyomino.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contega
+{
+    public interface IPolyomino
+    {
+        int width { get; }
+        int height { get; }
+        int id { get; }
+
+        int[,] GetRotated(int angle);
+    }
+}

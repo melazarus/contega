4f7c39c1c803956e96f938e0cca8463c7819c55b
Hans Polders <hans.polders@gfk.com>
2015-10-12 11:38:40 +0200

    Added IGamegrid.cs

---
+++ polyomino/IGameGrid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contega
+{
+    public interface IGameGrid
+    {
+        int Width { get; }
+        int Heigh { set; }
+
+        object[,] Grid { get; }
+    }
+}

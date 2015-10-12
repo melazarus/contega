a5d85e47deec08f477c49e7dbafcb960796f1719
Hans Polders <hans.polders@gfk.com>
2015-10-12 11:11:09 +0200

    added IContegaGame.cs

---
+++ polyomino/IContegaGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyomino
+{
+    public interface IContegaGame
+    {
+        /// <summary>
+        /// Advance the game one tick
+        /// </summary>
+        /// <returns>number of rows elliminated after tick</returns>
+        int Tick();
+
+        /// <summary>
+        /// Reset the game to its initial state
+        /// </summary>
+        void Reset();
+
+        object Grid { get; }
+
+        int Score { get; }
+
+        int Level { get; }
+    }
+}
